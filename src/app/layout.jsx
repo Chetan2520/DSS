07dc204553621cd6d9cef34ed31f74a552b1ed84
Digital Success Solution <digitalsuccesssolution9@gmail.com>
2026-07-23 17:09:42 +0530

    google tag manager

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -73,9 +73,26 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-1747CPHFPB');
           `}
         </Script>
+        <Script strategy="afterInteractive" id="gtm-script">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5HBHHBSR');`}
+        </Script>
         <FacebookPixel />
       </head>
       <body suppressHydrationWarning className="relative">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5HBHHBSR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ClientLayout>
           <LocalSchema />
           <MobileBottomNav />
